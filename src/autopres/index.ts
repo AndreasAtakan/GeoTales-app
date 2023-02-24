3adbfe2e13169da0a9ea25df98fec177d45b6dd5
Jonas Møller <jonas@moesys.no>
2023-02-24 17:43:14 +0100

    Initial implementation of alternate image clusters

--- src/autopres/index.ts
+++ src/autopres/index.ts
@@ -74,6 +74,138 @@ export class Pres {
 	}
 }
 
+class V3 {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    add(o: V3): V3 {
+        return new V3(this.x + o.x, this.y + o.y, this.z + o.z);
+    }
+
+    sub(o: V3): V3 {
+        return new V3(this.x - o.x, this.y - o.y, this.z - o.z);
+    }
+
+    scale(s: number): V3 {
+        return new V3(this.x * s, this.y * s, this.z * s);
+    }
+
+    len(): number {
+        return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+    }
+}
+
+class BBox3 {
+    min: V3;
+    max: V3;
+    core: V3;
+
+    constructor(min: V3, max: V3) {
+        this.min = min;
+        this.max = max;
+        this.core = max.sub(min).scale(0.5).add(min);
+    }
+
+    explode(): BBox3[] {
+        // Unit vectors to move boxes along
+        let xhat = new V3((this.min.x + this.max.x) / 2, 0, 0);
+        let yhat = new V3(0, (this.min.y + this.max.y) / 2, 0);
+        let zhat = new V3(0, 0, (this.min.z + this.max.z) / 2);
+
+        let ba = new BBox3(this.min, this.core);
+        let bb = ba.add(yhat);
+        let bc = bb.add(xhat);
+        let bd = ba.add(xhat);
+        let be = ba.add(zhat);
+        let bf = be.add(yhat);
+        let bg = bf.add(xhat);
+        let bh = be.add(xhat);
+
+        return [ba, bb, bc, bd, be, bf, bg, bh];
+    }
+
+    add(d: V3): BBox3 {
+        return new BBox3(this.min.add(d), this.max.add(d));
+    }
+
+    width(): number {
+        return this.max.x - this.min.x;
+    }
+
+    radius(): number {
+        return this.max.sub(this.min).len() / 2;
+    }
+
+    includes(p: V3) {
+        return (
+            p.x >= this.min.x && p.x <= this.max.x &&
+            p.y >= this.min.y && p.y <= this.max.y &&
+            p.z >= this.min.z && p.z <= this.max.z
+        )
+    }
+}
+
+class OTNode {
+    nodes: (OTNode | null)[];
+    pts: V3[] | null;
+    bbox: BBox3;
+
+    constructor(bbox: BBox3) {
+        this.nodes = [null, null, null, null, null, null, null, null];
+        this.pts = [];
+        this.bbox = bbox;
+    }
+
+    insert(pt: V3, t: OT) {
+        if (!this.pts) {
+            let nodes = this.nodes as OTNode[];
+            for (let node of nodes) {
+                node.insert(pt, t);
+            }
+        } else if (this.pts.length >= t.max_pts) {
+            let bboxes = this.bbox.explode();
+            this.pts.push(pt);
+            for (let i = 0; i < 8; i++) {
+                this.nodes[i] = new OTNode(bboxes[i]);
+            }
+            let nodes = this.nodes as OTNode[];
+            {
+                let pt;
+                while (pt = this.pts.pop()) {
+                    let min_dist = (1 / 0);
+                    let min_idx = 0;
+                    for (let i = 0; i < 8; i++) {
+                        let dist = bboxes[i].core.sub(pt).len();
+                        if (dist < min_dist) {
+                            min_dist = dist;
+                            min_idx = i;
+                        }
+                    }
+                    (nodes[min_idx].pts as V3[]).push(pt);
+                }
+            }
+            this.pts = null;
+        } else {
+            this.pts.push(pt);
+        }
+    }
+}
+
+class OT {
+    max_pts: number = 1;
+    min_width: number = 1;
+
+    insert(pt: V3) {
+    }
+}
+
 /**
  * Bounding box of image, essentially just a rectangle with sides equal
  * 2*IMG_RADIUS centered on the image GPS location.
